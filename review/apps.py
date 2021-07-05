700e98da8e2452f866aeaa61787a4969ec37fed2
tks007-github <takeshi.n.2020@gmail.com>
2021-07-05 23:25:06 +0900

    create
    ロガーの設定

--- review/apps.py
+++ review/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ReviewpostConfig(AppConfig):
+class ReviewConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'reviewpost'
+    name = 'review'
