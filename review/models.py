0a3a57fc11cc1328ecf0b59bb2b765fb26c9584d
tks007-github <takeshi.n.2020@gmail.com>
2021-07-10 11:35:49 +0900

    update

--- review/models.py
+++ review/models.py
@@ -10,7 +10,7 @@ class Reiview(models.Model):
     author = models.ForeignKey(CustomUser, verbose_name='投稿者', on_delete=models.PROTECT)
     title = models.CharField(verbose_name='タイトル', max_length=40)
     content = models.TextField(verbose_name='内容')
-    image = models.ImageField(verbose_name='写真1', blank=True, null=True)
+    image = models.ImageField(verbose_name='写真', blank=True, null=True)
     useful_review = models.IntegerField(verbose_name='いいね', null=True, blank=True, default=0)
     useful_review_record = models.TextField(verbose_name='講評')
     evaluation = models.CharField(verbose_name='評価', max_length=10, choices=EVALUATION_CHOICES)
