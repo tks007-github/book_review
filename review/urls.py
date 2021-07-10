e3544c7a700c2ea5509ce1ba9ffb962f77646966
tks007-github <takeshi.n.2020@gmail.com>
2021-07-10 22:06:48 +0900

    update

--- review/urls.py
+++ review/urls.py
@@ -7,5 +7,8 @@ from .import views
 app_name = 'review'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('inquiry/', views.InquiryView.as_view(), name='index'),
     path('review-list/', views.ReviewListView.as_view(), name='review_list'),
+    path('review-detail/<int:pk>/', views.ReviewDetailView.as_view(), name='review_detail'),
+    path('review-create/', views.ReviewCreateView.as_view(), name='review_create'),
 ]
--- review/views.py
+++ review/views.py
@@ -1,6 +1,8 @@
+from django.db.models.fields import PositiveBigIntegerField
 from django.shortcuts import render
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
+from .forms import InquiryForm, ReviewCreateForm
 
 import review
 
@@ -11,6 +13,9 @@ from review import models
 class IndexView(generic.TemplateView):
     template_name = 'index.html'
 
+class InquiryView(generic.FormView):
+    template_name = 'inquiry.html'
+    form_class = InquiryForm
 
 class ReviewListView(LoginRequiredMixin, generic.ListView):
     model = Reiview
@@ -18,5 +23,26 @@ class ReviewListView(LoginRequiredMixin, generic.ListView):
     paginate_by = 3
 
     def get_queryset(self):
-        reviews = Reiview.objects.order_by('-created_at')
+        reviews = Reiview.objects.filter(user=self.request.user).order_by('-created_at')
         return reviews
+
+class ReviewDetailView(LoginRequiredMixin, generic.DeleteView):
+    model = Reiview
+    template_name = 'review_detail.html'
+
+class ReviewCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Reiview
+    template_name = 'review_create.html'
+    form_class = ReviewCreateForm
+    success_url = reverse_lazy('review:review_list')
+
+    def form_valid(self, form):
+        review = form.save(commit=False)
+        review.user = self.request.user
+        review.save()
+        messages.success(self.request, 'レビューを作成しました。')
+        return super().form_valid(form)
+    
+    def form_valid(self, form):
+        messages.error(self.request, 'レビューの作成に失敗しました。')
+        return super().form_invalid(form)
