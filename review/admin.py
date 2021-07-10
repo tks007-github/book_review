fbbb596561392b6ae1fb5edfd5888452c6c5dab6
tks007-github <takeshi.n.2020@gmail.com>
2021-07-10 11:11:03 +0900

    update

--- review/admin.py
+++ review/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from .models import Reiview
+
 # Register your models here.
+admin.site.register(Reiview)
--- review/models.py
+++ review/models.py
@@ -1,3 +1,24 @@
+from accounts.models import CustomUser
 from django.db import models
 
 # Create your models here.
+
+EVALUATION_CHOICES = [('Good', 'Good'), ('Bad', 'Bad')]
+class Reiview(models.Model):
+    """レビューモデル"""
+
+    author = models.ForeignKey(CustomUser, verbose_name='投稿者', on_delete=models.PROTECT)
+    title = models.CharField(verbose_name='タイトル', max_length=40)
+    content = models.TextField(verbose_name='内容', blank=True, null=True)
+    image = models.ImageField(verbose_name='写真1', blank=True, null=True)
+    useful_review = models.IntegerField(verbose_name='いいね', null=True, blank=True, default=0)
+    useful_review_record = models.TextField()
+    evaluation = models.CharField(verbose_name='評価', max_length=10, choices=EVALUATION_CHOICES)
+    created_at = models.DateTimeField(verbose_name='作成日時', auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name='更新日時', auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'Review'
+
+        def __str__(self):
+            return self.title
