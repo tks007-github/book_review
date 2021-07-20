fd109d1e0f7910de1a184d87b2636f940a6d1545
tks007-github <takeshi.n.2020@gmail.com>
2021-07-21 06:26:17 +0900

    update

--- book_review/settings.py
+++ book_review/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n9r7uexd$njy4k)(+yox_-7y(ei(!qw1y-qy-xv8^5s+*m^h14'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
